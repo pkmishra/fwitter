342c6ffe2999d82206fad4f81d4047f6ebda8370
pkmishra <pkmishra@gmail.com>
2014-04-01 12:20:40 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fwitter::Application.config.secret_key_base = '4cfd9aa6cf958f2b018e8f3c1c7a670ec97ded8a8cd207c87abb96e7e7ad3d66b67888ed420c627ad7f237aeed95cb32c7b8e230161b3438e7c6647891844a45'
